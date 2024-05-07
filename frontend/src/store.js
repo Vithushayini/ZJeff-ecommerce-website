386317cfbc37fbed22e819398aeee8414a843af5
Vithushayini <vithushayini2000@gmail.com>
2024-05-07 08:08:29 +0530

    products slice created

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,10 @@
 import {combineReducers, configureStore} from "@reduxjs/toolkit";
 import {thunk} from "redux-thunk";
+import productsReducer from "./slices/productsSlice";
 
 
 const reducer=combineReducers({
-
+   produtsState: productsReducer
 })
 
 const store=configureStore({
